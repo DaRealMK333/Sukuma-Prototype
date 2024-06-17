29c3111142dfbfe5596ca5725e9a358c8d02f2c7
Benete21 <167325219+Benete21@users.noreply.github.com>
2024-06-17 19:42:09 +0200

    Final iteration with restart fixed

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.Serialization;
 
 public class UIManager : MonoBehaviour
@@ -41,9 +42,9 @@ public class UIManager : MonoBehaviour
 
             canvaGame.SetActive(true);
             canvaRestart.SetActive(false);
-
-            trackerScript.ResetScores();
-        }
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
         
       
     }
